788698dedde5289ecb6ec5a770ea5d7c965c552a
AhmedTarek73 <121679626+AhmedTarek73@users.noreply.github.com>
2022-12-30 21:09:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+bool flag = true;
+Console.WriteLine("Please Enter Frist Number:");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Please Enter Other Number:");
+int num1 = int.Parse(Console.ReadLine());
+
+if (num < 2)
+    num = 2;
+Console.WriteLine("The prime number is :");
+for (int a = num; a <= num1; a++)
+{
+    for (int n = 2; n <= num1; n++)
+    {
+        if (a != n && a % n == 0)
+        {
+            flag = false;
+            break;
+        }
+
+    }
+    if (flag)
+    {
+        Console.WriteLine("\t" + a);
+    }
+    flag = true;
+
+
+}
